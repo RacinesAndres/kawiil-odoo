50d1156b1154d69ada30b291cc87af8210a1ed9b
RacinesAndres <andres.racines@ibcaribe.com>
2024-07-11 21:22:12 +0000

    Probando modulo

--- motorcycle_registry/__manifest__.py
+++ motorcycle_registry/__manifest__.py
@@ -3,13 +3,16 @@
     'version': '0.0.1',
     'author': 'Andres Racines',
     'sequence': -100,
-    'category': 'Kawiil/Custom Module',
+    'category': 'Custom Module/Kawiil',
     'summary': 'Manage Registration of Motorcycles',
-    'description': """ Motorcycle Registry
-        ======================
-        This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand""",
-    'license': 'AGPL-3',
+    'description': """Motorcycle Registry 
+    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of 
+        the brand""",
+    'license': 'OPL-1',
+    'depensds': ['base'],
+    'data': [],
+    'demo': [],
     'application': True,
     'auto_install': False,
-    # 'images': ['static/description/banner.jpg'],
+    'website': 'https://github.com/RacinesAndres/kawiil-odoo-academy/tree/17.0-motorcycle-registry',
 }
