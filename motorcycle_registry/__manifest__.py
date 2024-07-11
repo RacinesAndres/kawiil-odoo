52746f993520e75556529f596f9039c496a52dfc
RacinesAndres <andres.racines@ibcaribe.com>
2024-07-11 04:20:01 +0000

    Descripción de los cambios realizados

---
+++ motorcycle_registry/__manifest__.py
@@ -0,0 +1,16 @@
+    {
+        "name": "Motorcycle Registry",
+        "summary": "Manage Registration of Motorcycles",
+        "description": """
+        Motorcycle Registry
+        ======================
+        This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand
+        """,
+        "author": "RacinesAndres",
+        "version": "0.0.1",
+        "license": "OPL-1",
+        "website": "https://github.com/RacinesAndres/kawiil-odoo-academy",
+        "depends": ["base"],
+        "data": [],
+        "aplication": True,     
+    }
