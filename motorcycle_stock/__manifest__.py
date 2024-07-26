41585f7aba3cd6f943fe644e7a02a5ee14ece6f7
RacinesAndres <andres.racines@ibcaribe.com>
2024-07-26 20:56:42 +0000

    Creando modulo nuevo Stock

---
+++ motorcycle_stock/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Motorcycle Stock',
+    'version': '0.0.1',
+    'author': 'Andres Racines',
+    'sequence': -100,
+    'category': 'Kawiil/Registry',
+    'license': 'OPL-1',
+    'depends': ['stock'],
+    'data': [],
+    'demo':[],
+    # 'data': ['views/procuct_template_view.xml'],
+    # 'demo': ['demo/product_demo.xml'],
+    'application': True,
+    'website': 'https://github.com/RacinesAndres',
+}
---
+++ motorcycle_stock/models/product.py
@@ -0,0 +1,30 @@
+from odoo import api, fields, models
+
+class Product(models.Model):
+    _inherit = 'product.template'
+
+    # horsepower = fields.Float(string="Caballos de fuerza")
+    # top_speed = fields.Float(string="Velocidad maxima")
+    # torque = fields.Float(string="Torque")
+    # battery_capacity =  fields.Selection(selection=[
+    #     ('xs', 'Small'),
+    #     ('0m', 'Medium'),
+    #     ('0l', 'Large'),
+    #     ('xl', 'Extra Large')], copy=False, string="Capacidad de Bateria")
+    # charge_time = fields.Float(string="Tiempo de carga")
+    # range = fields.Float(string="Rango")
+    # curb_weight = fields.Float(string="Peso")
+    # make = fields.Char(string="Marca")
+    # model = fields.Char(string="Modelo")
+    # year = fields.Char(string="Año")
+    # launch_date = fields.Date()
+
+    # detailed_type = fields.Seletion(selection_add=[('motorcycle', 'Motorcycle')], ondelete={'motorcycle': 'set product'})
+
+    # def _detailed_type_mapping(self):
+    #     type_mapping = super()._detailed_type_mapping()
+    #     type_mapping['motorcycle'] = 'product'
+    #     return type_mapping
+    
+    
+    
