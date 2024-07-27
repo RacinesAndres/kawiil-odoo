6201d5a6a1761fa04593ccffabaf17edd2a62d28
RacinesAndres <andres.racines@ibcaribe.com>
2024-07-27 05:31:47 +0000

    Add website y webmap

---
+++ motorcycle_map/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Motorcycle Map',
+    'version': '0.0.1',
+    'author': 'Andres Racines',
+    'sequence': -100,
+    'category': 'Kawiil/WebMap',
+    'license': 'OPL-1',
+    'depends': ['motorcycle_registry', 'web_map'],
+    'data': ['views/motorcycle_map_view.xml'],
+    'demo': [],
+    'application': True,
+    'website': 'https://github.com/RacinesAndres',
+}
