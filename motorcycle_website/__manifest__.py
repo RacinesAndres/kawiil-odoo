fd4a46cb84ce9f9578d0bd77487d4861f3f0fe68
RacinesAndres <andres.racines@ibcaribe.com>
2024-07-27 03:18:32 +0000

    ADD /compare

---
+++ motorcycle_website/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Motorcycle Website',
+    'version': '0.0.1',
+    'author': 'Andres Racines',
+    'sequence': -100,
+    'category': 'Kawiil/Website',
+    'license': 'OPL-1',
+    'depends': ['motorcycle_stock', 'website'],
+    'data': ['views/motorcycle_template_view.xml'],
+    'demo': [],
+    'application': True,
+    'website': 'https://github.com/RacinesAndres',
+}
